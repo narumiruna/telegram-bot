2ef51be6fc0e8ecc0385ab22f05e580c4d4ca8f2
narumi <toucans-cutouts0f@icloud.com>
2025-05-12 01:16:55 +0800

    configure langfuse

--- src/bot/cli.py
+++ src/bot/cli.py
@@ -3,10 +3,10 @@ from dotenv import find_dotenv
 from dotenv import load_dotenv
 
 from .bot import run_bot
-from .utils import configure_logfire
+from .utils import configure_langfuse
 
 
 def main():
     load_dotenv(find_dotenv(raise_error_if_not_found=True, usecwd=True))
-    configure_logfire()
+    configure_langfuse()
     typer.run(run_bot)
