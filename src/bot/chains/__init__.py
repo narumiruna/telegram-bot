9fe12834b3dde460887d31c1bbf95c06aec7fccf
narumi <toucans-cutouts0f@icloud.com>
2025-12-29 05:48:38 +0800

    delete product chain

--- src/bot/chains/__init__.py
+++ src/bot/chains/__init__.py
@@ -1,6 +1,5 @@
 from .formatter import format
 from .notes import create_notes
-from .product import extract_product
 from .summary import summarize
 from .translation import translate
 from .translation import translate_and_explain
