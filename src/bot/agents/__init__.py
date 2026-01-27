65a355ef48f5232c689ae8c3a865f68ae02dfb06
narumi <toucans-cutouts0f@icloud.com>
2026-01-28 03:30:39 +0800

    add build_filesystem_agent

--- src/bot/agents/__init__.py
+++ src/bot/agents/__init__.py
@@ -1,3 +1,7 @@
 from .chat import build_chat_agent
+from .filesystem import build_filesystem_agent
 
-__all__ = ["build_chat_agent"]
+__all__ = [
+    "build_chat_agent",
+    "build_filesystem_agent",
+]
---
+++ src/bot/agents/filesystem.py
@@ -0,0 +1,27 @@
+import os
+from collections.abc import AsyncIterator
+
+from agents import Agent
+from agents.mcp import MCPServerManager
+from agents.mcp import MCPServerStdio
+
+from ..model import get_openai_model
+
+
+async def build_filesystem_agent() -> AsyncIterator[Agent]:
+    servers = [
+        MCPServerStdio(
+            name="Filesystem Server, via npx",
+            params={
+                "command": "npx",
+                "args": ["-y", "@modelcontextprotocol/server-filesystem", os.getcwd()],
+            },
+        )
+    ]
+    async with MCPServerManager(servers) as manager:
+        agent = Agent(
+            "example-agent",
+            model=get_openai_model(),
+            mcp_servers=manager.active_servers,
+        )
+        yield agent
