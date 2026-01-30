015e642f989cd7669a21b243a8314b67fef872b1
narumi <toucans-cutouts0f@icloud.com>
2026-01-30 13:54:11 +0800

    feat: add safe_callback decorator to HelpCallback

--- src/bot/callbacks/help.py
+++ src/bot/callbacks/help.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 from aiogram.types import Message
 
 from .base import BaseCallback
+from .utils import safe_callback
 
 
 class HelpCallback(BaseCallback):
     def __init__(self, helps: list[str]) -> None:
         self.helps = helps
 
+    @safe_callback
     async def __call__(self, message: Message) -> None:
         await message.answer(
             "\n".join(self.helps),
