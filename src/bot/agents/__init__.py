de0d270cd9bdee5e5d1fb31a74b31b026c5ca31f
narumi <toucans-cutouts0f@icloud.com>
2026-02-04 23:04:15 +0800

    Add writer command

--- src/bot/agents/__init__.py
+++ src/bot/agents/__init__.py
@@ -1,7 +1,9 @@
 from .chat import build_chat_agent
 from .filesystem import build_filesystem_agent
+from .writer import build_writer_agent
 
 __all__ = [
     "build_chat_agent",
     "build_filesystem_agent",
+    "build_writer_agent",
 ]
--- src/bot/callbacks/__init__.py
+++ src/bot/callbacks/__init__.py
@@ -7,4 +7,19 @@ from .summary import summarize_callback
 from .ticker import query_ticker_callback
 from .translate import TranslationCallback
 from .utils import get_message_text
+from .writer import writer_callback
 from .youtube_search import search_youtube_callback
+
+__all__ = [
+    "echo_callback",
+    "ErrorCallback",
+    "file_callback",
+    "format_callback",
+    "get_message_text",
+    "help_callback",
+    "query_ticker_callback",
+    "search_youtube_callback",
+    "summarize_callback",
+    "TranslationCallback",
+    "writer_callback",
+]
