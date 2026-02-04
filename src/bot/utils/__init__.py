93b4acfc3e552f7f28b814a8f3da1967c6c2c225
narumi <toucans-cutouts0f@icloud.com>
2026-02-04 15:23:52 +0800

    move io utils

---
+++ src/bot/utils/__init__.py
@@ -0,0 +1,11 @@
+from .file_io import load_json
+from .file_io import save_json
+from .file_io import save_text
+from .url import load_url
+
+__all__ = [
+    "load_url",
+    "load_json",
+    "save_json",
+    "save_text",
+]
---
+++ src/bot/utils/file_io.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+from typing import Any
+
+
+def save_text(text: str, f: str) -> None:
+    with open(f, "w") as fp:
+        fp.write(text)
+
+
+def load_json(f: str | Path) -> Any:
+    path = Path(f)
+    if path.suffix != ".json":
+        raise ValueError(f"File {f} is not a json file")
+
+    with path.open(encoding="utf-8") as fp:
+        return json.load(fp)
+
+
+def save_json(data: Any, f: str) -> None:
+    with Path(f).open("w", encoding="utf-8") as fp:
+        json.dump(data, fp, ensure_ascii=False, indent=4)
