647991b7ecc73f951a8a0783217e513312c8d1d4
narumi <toucans-cutouts0f@icloud.com>
2026-02-04 16:03:25 +0800

    move obs utils

--- src/bot/utils/__init__.py
+++ src/bot/utils/__init__.py
@@ -1,6 +1,7 @@
 from .file_io import load_json
 from .file_io import save_json
 from .file_io import save_text
+from .observability import configure_logging
 from .page import async_create_page
 from .page import create_page
 from .retry import is_retryable_error
@@ -8,6 +9,7 @@ from .url import load_url
 
 __all__ = [
     "async_create_page",
+    "configure_logging",
     "create_page",
     "is_retryable_error",
     "load_json",
