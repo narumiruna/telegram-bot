168dbdd3b137f1b70012963e033ae2cbc84c490d
narumi <toucans-cutouts0f@icloud.com>
2026-02-05 03:18:15 +0800

    Remove PromptSpec class and related functions from prompting module

--- src/bot/core/__init__.py
+++ src/bot/core/__init__.py
@@ -1,11 +1,9 @@
 from .article import Article
 from .presentation import MessageResponse
 from .prompt_template import PromptTemplate
-from .prompting import PromptSpec
 
 __all__ = [
     "Article",
     "MessageResponse",
-    "PromptSpec",
     "PromptTemplate",
 ]
