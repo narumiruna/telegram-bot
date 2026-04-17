b93052c430e30d993533ef978764348eb3f9d93b
narumi <toucans-cutouts0f@icloud.com>
2026-04-17 16:02:37 +0800

    remove tools

--- src/bot/tools/__init__.py
+++ src/bot/tools/__init__.py
@@ -1,23 +1,6 @@
 from .award import search_award
-from .command import execute_command
-from .duckduckgo import web_search
-from .monster_hunter_weapon import draw_monster_hunter_weapon
 from .mortgage import compute_loan_details
 from .tarot import draw_tarot_card
 from .weblio import query_weblio
 from .wise import query_rate
 from .wise import query_rate_history
-from .yahoo_finance import query_ticker_from_yahoo_finance
-
-__all__ = [
-    "compute_loan_details",
-    "draw_monster_hunter_weapon",
-    "draw_tarot_card",
-    "execute_command",
-    "query_rate",
-    "query_rate_history",
-    "query_ticker_from_yahoo_finance",
-    "query_weblio",
-    "search_award",
-    "web_search",
-]
