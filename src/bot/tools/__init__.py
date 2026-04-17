73f11962c857d70e94fd769420c5f4ae05032371
narumi <toucans-cutouts0f@icloud.com>
2026-04-17 15:57:01 +0800

    remove datetime tool

--- src/bot/tools/__init__.py
+++ src/bot/tools/__init__.py
@@ -1,6 +1,5 @@
 from .award import search_award
 from .command import execute_command
-from .datetime import get_current_time
 from .duckduckgo import web_search
 from .monster_hunter_weapon import draw_monster_hunter_weapon
 from .mortgage import compute_loan_details
@@ -15,7 +14,6 @@ __all__ = [
     "draw_monster_hunter_weapon",
     "draw_tarot_card",
     "execute_command",
-    "get_current_time",
     "query_rate",
     "query_rate_history",
     "query_ticker_from_yahoo_finance",
